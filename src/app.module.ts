f209040316ca8679f47d677568dafe57513a7716
Harry EMARH <emarh.harry.code@gmail.com>
2024-04-09 02:30:39 +0200

    Connection to MongoDB with Mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,20 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+
 @Module({
-  imports: [RestaurantsModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(
+      'mongodb+srv://harry86:Davidvilla7@nestjs-restaurant.p1ugoij.mongodb.net/?retryWrites=true&w=majority&appName=NestJS-Restaurant',
+    ),
+    RestaurantsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
